32a80bbdeed0e69f3667ec0024ea5a9da904adf9
Johnlin2922 <johnlin2922@gmail.com>
2017-06-04 14:49:02 -0500

    test gradle.build works properly

---
+++ src/main/java/johnnyLin/is/awesome/Main.java
@@ -0,0 +1,9 @@
+package johnnyLin.is.awesome;
+
+public class Main{
+	
+	public static void main(String[] args){
+		
+		System.out.println("Hello, World!!!");
+	}
+}
---
+++ src/test/java/johnnyLin/is/awesome/chessTest.java
@@ -0,0 +1,13 @@
+package johnnyLin.is.awesome;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class chessTest{
+	
+	@Test
+	public void testGradleBuildWorks(){
+		
+		assertEquals(1, 1);
+	}
+}
