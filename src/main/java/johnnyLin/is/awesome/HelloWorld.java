5e32e7396fc3b87d9b53eb96aab108ce453c1857
Johnlin2922 <johnlin2922@gmail.com>
2017-06-04 14:31:03 -0500

    first commit

---
+++ src/main/java/johnnyLin/is/awesome/HelloWorld.java
@@ -0,0 +1,31 @@
+package johnnyLin.is.awesome;
+
+
+public class HelloWorld {
+    
+    public String showIt() {
+        return "hello world";
+    }
+    
+    public String reverseIt() {
+        return "dlrow olleh";
+    }
+    
+    public static void main(String[] args) {
+        HelloWorld h = new HelloWorld();
+        System.out.println(h.showIt());
+        System.out.println(h.reverseIt());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
